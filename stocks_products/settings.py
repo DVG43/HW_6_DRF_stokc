a7d0972ec00f5b79faa19b523f14d9648189bd25
Galuta Dmitrii <dvgaluta@gmail.com>
2022-06-08 00:22:14 +0300

    commit_2

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_stocks_products',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': 'stokcs_DB',
+        'USER': 'py48dmitriig',
+        'PASSWORD': 'luch1624',
     }
 }
 
