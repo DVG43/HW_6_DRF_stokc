a2bc0c7b041e8101807d9cba328c49cb80612310
Galuta Dmitrii <dvgaluta@gmail.com>
2022-06-17 01:08:11 +0300

    commit_9

--- logistic/views.py
+++ logistic/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+import django_filters.rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.filters import SearchFilter
